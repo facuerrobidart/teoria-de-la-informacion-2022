6c62b22188732733d626f276fa6e580228f482f1
facuerrobidart <85039144+facuerrobidart@users.noreply.github.com>
2022-09-24 13:38:59 -0300

    inecuacion de kraft

--- TP1/PARTE 2/main.c
+++ TP1/PARTE 2/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct nodo
 {
     char pal[8];
     int ocurrencia;
+    double prob;
 } nodo;
 
 void tresSimbolos();
@@ -13,11 +15,12 @@ void cincoSimbolos();
 void sieteSimbolos();
 void leerArchivo();
 void buscaYCuenta(nodo lista[], char *pal, int *tamLista);
-void mostrarResultados(nodo lista[], int strLen);
+void mostrarResultados(nodo lista[], int tamPalabra, int tamLista, int totalPalabras);
 void iniciaLista(nodo lista[]);
+void calculaKraft(int tamLista, int tamPalabra);
 
 int main(){
-    sieteSimbolos();
+    tresSimbolos();
     return 0;
 }
 
@@ -37,15 +40,19 @@ void leerArchivo(int tamPalabra) {
     FILE * arch = fopen("text.txt", "r");
     char pal[8];
     nodo lista[3500];
-    int *tamLista = 0;
+    int tamLista = 0;
+    int contPalabras = 0;
     iniciaLista(lista);
 
     while (!feof(arch)) {
         fgets(pal, (tamPalabra + 1) * sizeof(char), arch);
-        buscaYCuenta(lista, pal, &tamLista);
+        if (strlen(pal) == tamPalabra) {
+            buscaYCuenta(lista, pal, &tamLista);
+            contPalabras++;
+        }
     }
 
-    mostrarResultados(lista, tamPalabra);
+    mostrarResultados(lista, tamPalabra, tamLista, contPalabras);
 
     fclose(arch);
 }
@@ -53,6 +60,7 @@ void leerArchivo(int tamPalabra) {
 void iniciaLista(nodo lista[]) {
     for (int i = 0; i < 3500; i++) {
         lista[i].ocurrencia = 0;
+        lista[i].prob = 0;
         strcpy(lista[i].pal, "");
     }
 }
@@ -70,17 +78,44 @@ void buscaYCuenta(nodo lista[], char *pal, int *tamLista){
     if (!encontrado){
         strcpy(lista[i].pal, pal);
         lista[i].ocurrencia = 1;
-        *tamLista = i + 1;
+        (*tamLista)++;
     }
     else {
         lista[i].ocurrencia++;
     }
 }
 
-void mostrarResultados(nodo lista[], int strLen) {
-    for (int i = 0; i < 3500; i++) {
-        if (lista[i].ocurrencia != 0 && strlen(lista[i].pal) == strLen) {
-            printf("Palabra: %s Ocurrencias: %d \n", lista[i].pal, lista[i].ocurrencia);
-        }
+void mostrarResultados(nodo lista[], int tamPalabra, int tamLista, int cantPalabras) {
+    double cantidadDeInformacion = 0;
+    for (int i = 0; i < tamLista - 1; i++) {
+            lista[i].prob = (double) lista[i].ocurrencia / cantPalabras;
+            cantidadDeInformacion += log2(1/lista[i].prob);
+            //printf("Palabra: %s Ocurrencias: %4d Probalilidad: %0.4f \n", lista[i].pal, lista[i].ocurrencia, lista[i].prob);
     }
+
+    calculaKraft(tamLista, tamPalabra);
+}
+
+void calculaKraft(int tamLista, int tamPalabra) {
+    double kraft = 0;
+    for (int k = 0; k< tamLista; k++){
+        kraft += pow(3, (-1) * tamPalabra);
+        printf("Kraft: %0.4f \n", kraft);
+    }
+
+    printf("Cantidad de palabras: %d \n", tamLista);
+
+    if (kraft <= 1){
+        printf("El codigo de tamano %d cumple la inecuacion de Kraft/MacMillan (es compacto)\n", tamPalabra);
+    }
+    else {
+        printf("El codigo de tamano %d NO cumple la inecuacion de Kraft/MacMillan (es compacto)\n", tamPalabra);
+    }
+}
+
+float rendimiento(float entropia, float longitudMedia) {
+    return entropia / longitudMedia;
+}
+float reduncancia(float entropia, float longitudMedia) {
+    return 1 - rendimiento(entropia, longitudMedia);
 }
